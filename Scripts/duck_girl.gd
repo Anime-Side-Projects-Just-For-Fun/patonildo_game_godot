1bca05f179b216969c42f3818d982deceb606efb
Felipe Ribeiro <feliper.dev@gmail.com>
2024-08-24 23:42:31 -0300

    criando cenário e movimentação do pato

---
+++ Scripts/duck_girl.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var speed = 300
+
+const JUMP_VELOCITY = -400
+
+func _process(delta: float) -> void:
+	
+	if !is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		$Sprite2D.flip_h = false
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		$Sprite2D.flip_h = true
+	else:
+		velocity.x = 0
+
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	move_and_slide()
+	
