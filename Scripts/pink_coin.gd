4b1f5931bb9f1a26ecd2b535925244c5a2350cb5
Felipe Ribeiro <feliper.dev@gmail.com>
2024-08-25 00:38:01 -0300

    coins

--- Scripts/pink_coin.gd
+++ Scripts/pink_coin.gd
@@ -5,4 +5,6 @@ func _on_body_entered(body: Node2D) -> void:
 		queue_free() ### Remove objeto da cena
 		var coins = get_tree().get_nodes_in_group('PinkCoins')
 		print('Remaining coins: ', coins.size() - 1)
+		if coins.size() == 1:
+			print('Você coletou todos os PinkCoins!')
 	pass
