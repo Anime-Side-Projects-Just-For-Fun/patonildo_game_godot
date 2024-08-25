684676909b023e4c55e787b816f779f7edba32ac
Felipe Ribeiro <feliper.dev@gmail.com>
2024-08-25 00:30:31 -0300

    adicionando coleta de colecionáveis

--- Scripts/pink_coin.gd
+++ Scripts/pink_coin.gd
@@ -3,4 +3,6 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == 'Patonildo':
 		queue_free() ### Remove objeto da cena
+		var coins = get_tree().get_nodes_in_group('PinkCoins')
+		print('Remaining coins: ', coins.size() - 1)
 	pass
